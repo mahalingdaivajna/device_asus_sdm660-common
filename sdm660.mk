f7cdcb85bddf0b03f5236f6f3e04e81cc02c6f4d
Danny Baumann <dannybaumann@web.de>
2019-06-22 14:26:09 +0000

    sdm660-common: add touch hal hidl for gestures
    Edits:
     - adapt code to work with single node
     - add all supported gestures
     - add custom gesture strings
     - set custom default actions for each gesture
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -456,6 +456,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.mock \
     thermal.sdm660
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00T
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
